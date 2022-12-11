a18beccb1cf856cdd2c0d045694d912112d7b42b
Zainab <zainabp491@gmail.com>
2022-12-11 06:36:28 +0530

    stting up passport.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const expressLayouts= require('express-ejs-layouts');
 
 const db= require('./config/mongoose');
 
+//used for session Cookies
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -19,15 +24,30 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-//use express router
-app.use('/',require('./routes'));
+
 
 //set up the view engine
 app.set('view engine','ejs');
 app.set('views', './views');
 
 
+app.use(session({
+    name: 'codial',
+    //change the secret before deployment in production
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
 
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//use express router
+app.use('/',require('./routes'));
 
 
 app.listen(port,function(err){
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express= require('express');
 
 const router= express.Router();
+const passport=require('passport');
 
 const usersController= require('../controllers/users_controller');
 router.get('/profile',usersController.profile);
@@ -10,5 +11,11 @@ router.get('/sign-up' ,usersController.signUp);
 router.get('/sign-in',usersController.signIn);
 
 router.post('/create', usersController.create);
+
+//use passport as a middleware to authentication
+router.post('/create-session',passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'},
+) ,usersController.createSession);
 
 module.exports=router;
