386692a04817e265a873d592ef0bab0bee65e3e6
Zainab <zainabp491@gmail.com>
2022-12-14 02:07:51 +0530

    displaying posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,27 @@
-module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-    return  res.render('home', {
-          title: "Home"
-    }) ;
+const Post = require('../models/post');
 
+
+module.exports.home = function(req, res){
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+
+    // Post.find({}, function(err, posts){
+
+    //     return  res.render('home', {
+    //         title: "Codial | Home",
+    //         posts: posts
+    //   }) ;
+
+    // });
+
+    //populate the user of the each post
+   
+    Post.find({}).populate('user').exec(function(err,posts){
+        return  res.render('home', {
+            title: "Codial | Home",
+            posts: posts
+      }) ;
+    });
 };
 
 //module.exports.actionName=function(req,res);
