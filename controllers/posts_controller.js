0806361d6d01883418c1945b11594a3236879224
Zainab <zainabp491@gmail.com>
2022-12-16 23:22:37 +0530

    Deleting Commit

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create=function(req,res){
     Post.create({
@@ -6,6 +7,22 @@ module.exports.create=function(req,res){
         user: req.user._id
     }, function(err, post){
         if(err){console.log('error in  creating a post'); return;}
+
         return res.redirect('back');
+    });
+}
+
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        //.id means converting the object id  into String
+         if(post.user==req.user.id){
+            post.remove();
+             Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+             });
+         }else{
+            return res.redirect('back');
+         }
     });
 }
