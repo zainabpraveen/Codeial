df675270d1dd91adced6d9b1a5cdf9d931d853d2
Zainab <zainabp491@gmail.com>
2023-01-02 18:47:46 +0530

    Setting Up Passport JWT

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const db= require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
 const  flash = require('connect-flash');
 const customMware = require('./config/middleware');
-
 app.use(sassMiddleware({
  src: './assets/scss',
  dest: './assets/css',
